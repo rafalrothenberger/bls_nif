e3fb98bc34c9dd4c69df5f2a63c1efe620e72d3d
Rafał Rothenberger <rafal.rothenberger@gmail.com>
2019-11-07 22:45:08 +0100

    Return strings instead of charlist

--- lib/bls_nif.ex
+++ lib/bls_nif.ex
@@ -3,58 +3,54 @@ defmodule BlsNif do
 
   def g1neg(a) when is_binary(a) do
     a = to_charlist(a)
-    nif_g1neg(a)
+    to_string(nif_g1neg(a))
   end
 
   def g2neg(a) when is_binary(a) do
     a = to_charlist(a)
-    nif_g2neg(a)
+    to_string(nif_g2neg(a))
   end
 
   def g1dbl(a) when is_binary(a) do
     a = to_charlist(a)
-    nif_g1dbl(a)
+    to_string nif_g1dbl(a)
   end
 
   def g2dbl(a) when is_binary(a) do
     a = to_charlist(a)
-    nif_g2dbl(a)
+    to_string nif_g2dbl(a)
   end
 
   def g1add(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g1add(a,b)
+    to_string nif_g1add(a,b)
   end
 
   def g2add(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g2add(a,b)
+    to_string nif_g2add(a,b)
   end
 
   def g1sub(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g1sub(a,b)
+    to_string nif_g1sub(a,b)
   end
-
-  def g1sub(a,b), do: nif_g1sub(a,b)
 
   def g2sub(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g2sub(a,b)
+    to_string nif_g2sub(a,b)
   end
-
-  def g2sub(a,b), do: nif_g2sub(a,b)
 
   def g1mul(a, b) when is_integer(b), do: g1mul(a, Integer.to_string(b))
 
   def g1mul(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g1mul(a,b)
+    to_string nif_g1mul(a,b)
   end
 
   def g2mul(a,b) when is_integer(b), do: g2mul(a, Integer.to_string(b))
@@ -62,17 +58,17 @@ defmodule BlsNif do
   def g2mul(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_g2mul(a,b)
+    to_string nif_g2mul(a,b)
   end
 
   def g1hash(s) when is_binary(s) do
-    nif_g1hash(s, byte_size(s))
+    to_string nif_g1hash(s, byte_size(s))
   end
 
   def g1hash(_), do: raise "g1hash/1 requires binary on input"
 
   def g2hash(s) when is_binary(s) do
-    nif_g2hash(s, byte_size(s))
+    to_string nif_g2hash(s, byte_size(s))
   end
 
   def g2hash(_), do: raise "g2hash/1 requires binary on input"
@@ -80,6 +76,6 @@ defmodule BlsNif do
   def pairing(a,b) when is_binary(a) and is_binary(b) do
     a = to_charlist(a)
     b = to_charlist(b)
-    nif_pairing(a,b)
+    to_string nif_pairing(a,b)
   end
 end
